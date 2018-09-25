911b0cac44847bd3c39fe19c70f6f96730f7cda8
Matthew Deacon <matthewmdeacon@gmail.com>
2018-09-25 23:28:55 +0200

    Got scroll on focus working ios

--- app/screens/main/receiveScreen.js
+++ app/screens/main/receiveScreen.js
@@ -6,6 +6,8 @@ import {
   Dimensions,
   TouchableHighlight,
   Clipboard,
+  KeyboardAvoidingView,
+  ScrollView,
 } from 'react-native';
 import { connect } from 'react-redux';
 import {
@@ -17,6 +19,7 @@ import {
   toggleAccountField,
   resetReceive,
 } from '../../redux/actions';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import { Toast } from 'native-base';
 import Header from './../../components/header';
@@ -80,126 +83,140 @@ class ReceiveScreen extends Component {
           noShadow={crypto}
           title="Receive"
         />
-        <View style={{ padding: 8, width: '100%' }}>
-          <Output label={''} value={receive.value} copy />
-          <TouchableHighlight
-            underlayColor={'white'}
-            activeOpacity={0.2}
-            onPress={() => this._copyQR(receive)}>
-            <Image
-              style={{ width: 300, height: 220, alignSelf: 'center' }}
-              source={{
-                uri:
-                  'https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=&choe=UTF-8' +
-                  encodeURIComponent(receive.value),
-              }}
-            />
-          </TouchableHighlight>
-          {receive.buttons ? (
-            <View style={{ flexDirection: 'row' }}>
-              {receive.email ? (
-                <View style={{ flex: 1 }}>
-                  <Button
-                    color={receive.type === 'email' ? 'primary' : 'secondary'}
-                    onPress={() => setReceiveType('email')}
-                    label="EMAIL"
-                    size="small"
-                    round
-                    containerStyle={{ marginBottom: 0 }}
-                  />
-                </View>
-              ) : null}
-              {receive.mobile ? (
-                <View style={{ flex: 1 }}>
-                  <Button
-                    color={receive.type === 'mobile' ? 'primary' : 'secondary'}
-                    onPress={() => setReceiveType('mobile')}
-                    label="MOBILE"
-                    size="small"
-                    round
-                    containerStyle={{ marginBottom: 0 }}
-                  />
-                </View>
-              ) : null}
-              {receive.crypto ? (
-                <View style={{ flex: 1 }}>
-                  <Button
-                    color={receive.type === 'crypto' ? 'primary' : 'secondary'}
-                    onPress={() => setReceiveType('crypto')}
-                    label="CRYPTO"
-                    size="small"
-                    round
-                    containerStyle={{ marginBottom: 0 }}
-                  />
-                </View>
-              ) : null}
-            </View>
-          ) : null}
-          <Input
-            key="amount"
-            placeholder="e.g. 10"
-            label={
-              'Amount' +
-              (receive && receive.currency && receive.currency.currency
-                ? ' [' + receive.currency.currency.symbol + ']'
-                : '')
-            }
-            reference={input => (this.amountInput = input)}
-            keyboardType={Platform.OS === 'ios' ? 'decimal-pad' : 'phone-pad'}
-            value={receive.amount}
-            onChangeText={value =>
-              updateAccountField({ prop: 'receiveAmount', value })
-            }
-            returnKeyType="next"
-            onSubmitEditing={() =>
-              true ? this.memoInput.focus() : this.noteInput.focus()
-            }
-            type={'money'}
-            // precision={transaction.currency.currency.divisibility}
-            // unit={transaction.currency.currency.symbol + ' '}
-            toggleCheck={() => this.props.toggleAccountField('receiveAmount')}
-            checked={receive.amountSelected}
-          />
-          <Input
-            key="memo"
-            placeholder="Memo"
-            label="Memo"
-            value={receive.memo}
-            onChangeText={value =>
-              updateAccountField({ prop: 'receiveMemo', value })
-            }
-            reference={input => (this.memoInput = input)}
-            returnKeyType="next"
-            // autoFocus
-            onSubmitEditing={() => this.noteInput.focus()}
-            toggleCheck={() => this.props.toggleAccountField('receiveMemo')}
-            checked={receive.memoSelected}
-          />
-          <Input
-            key="note"
-            placeholder="e.g. Rent"
-            label="Note"
-            value={receive.note}
-            onChangeText={value =>
-              updateAccountField({ prop: 'receiveNote', value })
-            }
-            // inputError={sendError}
-            reference={input => (this.noteInput = input)}
-            multiline
-            returnKeyType="next"
-            toggleCheck={() => this.props.toggleAccountField('receiveNote')}
-            checked={receive.noteSelected}
-          />
-          {receive.currency ? (
-            <Checkbox
-              title={'Currency: ' + receive.currency.currency.code}
-              toggleCheck={() =>
-                this.props.toggleAccountField('receiveCurrency')
+        <KeyboardAwareScrollView
+          resetScrollToCoords={{ x: 0, y: 0 }}
+          contentContainerStyle={{ padding: 8 }}
+          extraScrollHeight={80}
+          keyboardOpeningTime={150}>
+          <View style={{ flex: 1 }}>
+            <Output label={''} value={receive.value} copy />
+            <TouchableHighlight
+              underlayColor={'white'}
+              activeOpacity={0.2}
+              onPress={() => this._copyQR(receive)}>
+              <Image
+                style={{
+                  width: SCREEN_WIDTH - 64,
+                  height: SCREEN_WIDTH - 128,
+                  alignSelf: 'center',
+                }}
+                source={{
+                  uri:
+                    'https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=&choe=UTF-8' +
+                    encodeURIComponent(receive.value),
+                }}
+              />
+            </TouchableHighlight>
+            {receive.buttons ? (
+              <View style={{ flexDirection: 'row' }}>
+                {receive.email ? (
+                  <View style={{ flex: 1 }}>
+                    <Button
+                      color={receive.type === 'email' ? 'primary' : 'secondary'}
+                      onPress={() => setReceiveType('email')}
+                      label="EMAIL"
+                      size="small"
+                      round
+                      containerStyle={{ marginBottom: 0 }}
+                    />
+                  </View>
+                ) : null}
+                {receive.mobile ? (
+                  <View style={{ flex: 1 }}>
+                    <Button
+                      color={
+                        receive.type === 'mobile' ? 'primary' : 'secondary'
+                      }
+                      onPress={() => setReceiveType('mobile')}
+                      label="MOBILE"
+                      size="small"
+                      round
+                      containerStyle={{ marginBottom: 0 }}
+                    />
+                  </View>
+                ) : null}
+                {receive.crypto ? (
+                  <View style={{ flex: 1 }}>
+                    <Button
+                      color={
+                        receive.type === 'crypto' ? 'primary' : 'secondary'
+                      }
+                      onPress={() => setReceiveType('crypto')}
+                      label="CRYPTO"
+                      size="small"
+                      round
+                      containerStyle={{ marginBottom: 0 }}
+                    />
+                  </View>
+                ) : null}
+              </View>
+            ) : null}
+            <Input
+              key="amount"
+              placeholder="e.g. 10"
+              label={
+                'Amount' +
+                (receive && receive.currency && receive.currency.currency
+                  ? ' [' + receive.currency.currency.symbol + ']'
+                  : '')
               }
-              value={receive.currencySelected}
+              reference={input => (this.amountInput = input)}
+              keyboardType={Platform.OS === 'ios' ? 'decimal-pad' : 'phone-pad'}
+              value={receive.amount}
+              onChangeText={value =>
+                updateAccountField({ prop: 'receiveAmount', value })
+              }
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                true ? this.memoInput.focus() : this.noteInput.focus()
+              }
+              type={'money'}
+              // precision={transaction.currency.currency.divisibility}
+              // unit={transaction.currency.currency.symbol + ' '}
+              toggleCheck={() => this.props.toggleAccountField('receiveAmount')}
+              checked={receive.amountSelected}
             />
-          ) : null}
-        </View>
+            <Input
+              key="memo"
+              placeholder="Memo"
+              label="Memo"
+              value={receive.memo}
+              onChangeText={value =>
+                updateAccountField({ prop: 'receiveMemo', value })
+              }
+              reference={input => (this.memoInput = input)}
+              returnKeyType="next"
+              // autoFocus
+              onSubmitEditing={() => this.noteInput.focus()}
+              toggleCheck={() => this.props.toggleAccountField('receiveMemo')}
+              checked={receive.memoSelected}
+            />
+            <Input
+              key="note"
+              placeholder="e.g. Rent"
+              label="Note"
+              value={receive.note}
+              onChangeText={value =>
+                updateAccountField({ prop: 'receiveNote', value })
+              }
+              // inputError={sendError}
+              reference={input => (this.noteInput = input)}
+              multiline
+              returnKeyType="next"
+              toggleCheck={() => this.props.toggleAccountField('receiveNote')}
+              checked={receive.noteSelected}
+            />
+            {receive.currency ? (
+              <Checkbox
+                title={'Currency: ' + receive.currency.currency.code}
+                toggleCheck={() =>
+                  this.props.toggleAccountField('receiveCurrency')
+                }
+                value={receive.currencySelected}
+              />
+            ) : null}
+          </View>
+        </KeyboardAwareScrollView>
       </View>
     );
   }
@@ -208,11 +225,7 @@ class ReceiveScreen extends Component {
 const styles = {
   container: {
     flex: 1,
-    flexDirection: 'column',
     backgroundColor: 'white',
-    // alignItems: 'center',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
   },
   text: {
     fontSize: 16,
